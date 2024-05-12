797c0b0d991c722ec3d26eb97362e0125493a30a
victor-code <vdcloayza@gmail.com>
2024-05-11 21:35:55 -0400

    example 1 added

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,16 @@
+<script>
+  let count = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button on:click={increment}>
+  count is {count}
+</button>
+
+{#if count > 10}
+<p>{count} is greater than 10</p>
+{:else}
+<p>{count} is between 0 and 10</p>
+{/if}
