76926ba10a96f2d44a6c6e72e2760eaf7d4af2d3
victor-code <vdcloayza@gmail.com>
2024-05-12 20:47:15 -0400

    examples added/maybe indexedDB

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,14 +1,37 @@
 <script>
-  let count = 0
+  import item from './data.json';
+  let count = 0;
+
   const increment = () => {
     count += 1
   }
+
+  $:doubled=count*2;
+
+  $:console.log(`the count is ${count}`)
+
+let numbers = [1, 2, 3, 4];
+
+function addNumber() {
+  numbers = [...numbers, numbers.length + 1];
+}
+
+
+$: sum = numbers.reduce((total, currentNumber) => total + currentNumber, 0);
 </script>
+
+
+
+<p>{numbers.join(' + ')} = {sum}</p>
+
+<button on:click={addNumber}>
+	Add a number
+</button>
 
 <button on:click={increment}>
   count is {count}
 </button>
-
+<p>{count} doubled is {doubled}</p>
 {#if count > 10}
 <p>{count} is greater than 10</p>
 {:else}
